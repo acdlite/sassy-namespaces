fc9ed9e4865b6cb52dcffc30386c3403c77f6d50
Andrew Clark <acdlite@me.com>
2014-03-18 15:14:07 -0400

    Bump version

--- lib/sassy-namespaces.rb
+++ lib/sassy-namespaces.rb
@@ -5,6 +5,6 @@ extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Compass::Frameworks.register('sassy-namespaces', :path => extension_path)
 
 module SassyNamespaces
-  VERSION = "0.1.3"
+  VERSION = "0.1.4"
   DATE = "2014-03-18"
 end
