cd1a813ab048f4349148643ddf21f8f4f8e06d11
Andrew Clark <acdlite@me.com>
2014-03-18 14:52:56 -0400

    Fixes

--- lib/sassy-namespaces.rb
+++ lib/sassy-namespaces.rb
@@ -2,9 +2,9 @@ require 'compass'
 require 'sassy-maps'
 
 extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
-Compass::Frameworks.register('SassyNamespaces', :path => extension_path)
+Compass::Frameworks.register('sassy-namespaces', :path => extension_path)
 
 module SassyNamespaces
-  VERSION = "0.1.1"
-  DATE = "2014-03-17"
+  VERSION = "0.1.3"
+  DATE = "2014-03-18"
 end
