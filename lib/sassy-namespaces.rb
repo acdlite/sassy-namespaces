ca3274fd1ec1a442b642d221029c640971523dde
Andrew Clark <acdlite@me.com>
2014-03-20 11:49:01 -0400

    Fix check for argument length in namespace-get

--- lib/sassy-namespaces.rb
+++ lib/sassy-namespaces.rb
@@ -5,6 +5,6 @@ extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Compass::Frameworks.register('sassy-namespaces', :path => extension_path)
 
 module SassyNamespaces
-  VERSION = "0.1.5"
-  DATE = "2014-03-18"
+  VERSION = "0.1.7"
+  DATE = "2014-03-20"
 end
